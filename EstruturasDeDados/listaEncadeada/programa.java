48ac081757cf0a0c3f39ef240e1272d16e75c746
RamosRaphael22 <102265064+RamosRaphael22@users.noreply.github.com>
2024-04-17 16:12:31 -0300

    Update programa.java

--- EstruturasDeDados/listaEncadeada/programa.java
+++ EstruturasDeDados/listaEncadeada/programa.java
@@ -3,21 +3,36 @@ package estudo;
 public class programa {
 
 	public static void main(String[] args) {
-
+		
 		listaEncadeada lista = new listaEncadeada();
-		 lista.adicionar("AC");
-		 lista.adicionar("BA");
-		 lista.adicionar("CE");
-		 lista.adicionar("DF");
-		 lista.adicionar("PE");
-		 System.out.println("tamanho: " + lista.getTamanho());	
-		 System.out.println("primero: " + lista.getPrimeiro().getValor());
-		 System.out.println("ultimo: " + lista.getUltimo().getValor());
-		 System.out.println(lista.get(0).getValor());
-		 System.out.println(lista.get(1).getValor());
-		 System.out.println(lista.get(2).getValor());
-		 System.out.println(lista.get(3).getValor());
-		 System.out.println(lista.get(4).getValor());
+		
+		//adicionar elementos
+		
+		lista.adicionar("AC");
+		lista.adicionar("BA");
+		lista.adicionar("CE");
+		lista.adicionar("DF");
+		lista.adicionar("PE");
+		System.out.println("tamanho: " + lista.getTamanho());	
+		System.out.println("primero: " + lista.getPrimeiro().getValor());
+		System.out.println("ultimo: " + lista.getUltimo().getValor());
+		 
+		for(int i=0; i<lista.getTamanho(); i++) {
+			System.out.println(lista.get(i).getValor());
+		}
+		 
+		//remover elementos
+		
+		lista.remover("AC");
+		lista.remover("BA");
+		lista.remover("CE");
+		lista.remover("DF");
+		lista.remover("PE");
+		
+		for(int i=0; i<lista.getTamanho(); i++) {
+			System.out.println(lista.get(i).getValor());
+		}
+		System.out.println("tamanho da lista apos remoçao: " + lista.getTamanho());
 	}
 
 }
